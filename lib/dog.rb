ddebb84fa3e6c00c3e422270ac6fde289a87d007
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-17 15:12:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 require "pry"
 class Dog 
   attr_accessor :name, :breed, :id
-  def initialize(hash,id=nil)
+  def initialize(id: nil, name:, breed:)
     @id = id
-    @name = hash[:name]
-    @breed = hash[:breed]
+    @name = name
+    @breed = breed
   end
   def self.create_table
     sql = <<-SQL
@@ -35,6 +35,30 @@ class Dog
     id = row[0]
     name = row[1]
     breed = row[2]
-    Dog.new(id: id, name: name, breed: breed)
+    self.new(id: id, name: name, breed: breed)
+  end
+  def self.find_by_id(id)
+    
+    DB[:conn].execute( "SELECT * FROM dogs WHERE id = ? LIMIT 1",id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  def self.find_or_create_by(name:,breed:)
+  
+     dog = DB[:conn].execute(" SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+     
+     if !dog.empty?
+       dog_data = dog[0]
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else 
+      self.create(name: name, breed: breed)
+    end
+  end
+  def self.find_by_name(name)
+    dog=DB[:conn].execute("SELECT * FROM dogs WHERE name = ?",name).flatten
+    self.new_from_db(dog)
+  end
+  def update
+     DB[:conn].execute('UPDATE dogs SET breed = ?, name = ? WHERE id = ?', self.breed, self.name, self.id)
   end
 end
